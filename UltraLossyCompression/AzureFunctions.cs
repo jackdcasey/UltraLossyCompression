5e8491e0ccadf63842d41b6609ead8ec586cf40c
Jack Casey <jackbonjour@gmail.com>
2019-08-13 23:24:05 -0700

    Finalizing things

---
+++ UltraLossyCompression/AzureFunctions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Search.ImageSearch;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+
+namespace UltraLossyCompression
+{
+    public static class AzureFunctions
+    {
+        const string VisionSubscriptionKey = "SUBSCRIPTIONKEY";
+        const string SearchSubscriptionKey = "SUBSCRIPTIONKEY";
+
+        // Given an image file, converts into a string description of that image
+        public static async Task<string> GetDescriptionFromImage(string filePath)
+        {
+            ComputerVisionClient computerVision =
+                new ComputerVisionClient(new Microsoft.Azure.CognitiveServices.Vision.ComputerVision.ApiKeyServiceClientCredentials(VisionSubscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+
+            computerVision.Endpoint = "https://westus.api.cognitive.microsoft.com";
+
+            var features = new List<VisualFeatureTypes>() { VisualFeatureTypes.Description };
+
+            using (Stream imageStream = File.OpenRead(filePath))
+            {
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(imageStream, features);
+
+                return analysis.Description.Captions.FirstOrDefault().Text;
+            }
+        }
+
+        // Given a description and filepath, converts a description into a full size image
+        public static void DownloadImageFromDescription(string description, string filePath)
+        {
+            var client = new ImageSearchClient(new Microsoft.Azure.CognitiveServices.Search.ImageSearch.ApiKeyServiceClientCredentials(SearchSubscriptionKey));
+
+            var imageresults = client.Images.SearchAsync(description).Result;
+
+            string url = imageresults.Value.ElementAt(new Random().Next(imageresults.Value.Count)).ContentUrl;
+
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(new Uri(url));
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    using (var newImage = Image.FromStream(mem))
+                    {
+                        switch (Path.GetExtension(filePath).ToLower())
+                        {
+                            case ".jpg":
+                                newImage.Save(filePath, ImageFormat.Jpeg);
+                                break;
+                            case ".png":
+                                newImage.Save(filePath, ImageFormat.Png);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UltraLossyCompression/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace UltraLossyCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new[] { "/Users/jack/Projects/UltraLossyCompression/UltraLossyCompression/TestImages/DogPhoto.png.ultralossy" };
+
+            if (args.Length != 1 && !File.Exists(args[0]))
+            {
+                Console.WriteLine("Please specify a valid file");
+                return;
+            }
+
+            string inputPath = args[0];
+
+            if (!new[] { ".jpg", ".png", ".ultralossy" }.Contains(Path.GetExtension(inputPath).ToLower()))
+            {
+                Console.WriteLine("Image not a valid type, please select a .jpg or .png");
+                return;
+            }
+
+            Console.WriteLine($"Converting {inputPath}");
+
+            if (Path.GetExtension(inputPath).Equals(".ultralossy"))
+            {
+                string imageDescription = File.ReadAllText(inputPath);
+
+                string outputPath = Path.ChangeExtension(inputPath, null);
+
+                Console.WriteLine($"Writing output to {outputPath}");
+
+                AzureFunctions.DownloadImageFromDescription(imageDescription, outputPath);
+
+                File.Delete(inputPath);
+            }
+            else
+            {
+                string imageDescription = AzureFunctions.GetDescriptionFromImage(inputPath).Result;
+
+                string outputPath = String.Concat(inputPath, ".ultralossy");
+
+                Console.WriteLine($"Writing output to {outputPath}");
+
+                File.WriteAllText(outputPath, imageDescription);
+
+                File.Delete(inputPath);
+            }
+        }
+    }
+}
